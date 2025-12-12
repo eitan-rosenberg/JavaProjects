014e0bb3c6a6e32a3bdbdfca9ed75597a95b2bbe
Eitan Rosenberg <48506054+eitan-rosenberg@users.noreply.github.com>
2025-12-12 20:19:23 +0200

    Delete MyLogger directory

--- MyLogger/src/main/java/myLogging/myHandlerRecords/MyHandlerRecord.java
+++
@@ -1,9 +0,0 @@
-package myLogging.myHandlerRecords;
-
-import java.util.logging.Handler;
-
-import myLogging.myHandlerTypes.MyHandlerType;
-
-public record MyHandlerRecord(Handler handler, MyHandlerType MyHandlerType) {
-
-}
